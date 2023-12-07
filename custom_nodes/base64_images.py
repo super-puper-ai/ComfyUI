e1d95343f54cddf652b9d801214c28630eee0460
Nil1208 <don007danny@gmail.com>
2023-12-07 14:33:15 +0300

    feat[block]: new block base64toimage

--- custom_nodes/base64_images.py
+++ custom_nodes/base64_images.py
@@ -1,5 +1,6 @@
+import torch
 import base64
-from PIL import Image
+from PIL import Image, ImageOps
 import numpy as np
 from io import BytesIO
 import requests
@@ -75,15 +76,49 @@ class SendBufferedImages:
         return ()
 
 
+class Base64ToImage:
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "base64Str": ("STRING", {"multiline": True}),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE","MASK")
+    FUNCTION = "base64_to_image"
+
+    OUTPUT_NODE = True
+
+    CATEGORY = "image"
+
+    def base64_to_image(self, base64Str):
+        _bytes = base64.decodebytes(bytes(base64Str, 'utf-8'))
+        i = Image.open(BytesIO(_bytes))
+        i = ImageOps.exif_transpose(i)
+        image = i.convert("RGB")
+        image = np.array(image).astype(np.float32) / 255.0
+        image = torch.from_numpy(image)[None,]
+        if 'A' in i.getbands():
+            mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+            mask = 1. - torch.from_numpy(mask)
+        else:
+            mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
+        return (image, mask.unsqueeze(0))
+
+
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
     "ImageToBuffer": ImageToBuffer,
-    "SendBufferedImages": SendBufferedImages
+    "SendBufferedImages": SendBufferedImages,
+    "Base64ToImage": Base64ToImage,
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ImageToBuffer": "Image to buffer",
-    "SendBufferedImages": "Send buffered images"
+    "SendBufferedImages": "Send buffered images",
+    "Base64ToImage": "Base64 To Image",
 }
